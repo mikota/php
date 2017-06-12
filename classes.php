17266187d8dc3270766ae2cd862318735dc6d3ce
lovro g <lovrogrgic0@gmail.com>
2017-06-12 22:02:57 +0200

    init reservation class

--- classes.php
+++ classes.php
@@ -1,1 +1,15 @@
-test
+<?php
+	class Reservation {
+		var $id,$grupa_id,$dt_start,$dt_end,$dt_created,$duration_mins,$confirmed;
+		//taken directly from db
+
+		function __construct(){
+			$this->id = $this->returnNum(3);
+		}
+		function returnNum($num){
+			return $num;
+		}
+	}
+	$obj = new Reservation;
+	echo $obj->id;
+?>
